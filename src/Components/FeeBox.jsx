0c71d3cd272ffa1f0dcbb50e54fbbe05627b0095
usman4222 <usman4243ch@gmail.com>
2024-10-21 20:40:19 +0500

    Add fee box

---
+++ src/Components/FeeBox.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FeeBox = ({ title, total }) => {
+  return (
+    <div>
+      <div
+        style={{ boxShadow: '0px 1px 3px 0px #0051AF1A' }}
+        className="border border-[#E2E8F0] rounded-[10px] pl-5 pr-10 py-4"
+      >
+        <h6 className="font-montserrat text-[#4E5564] text-[14px] font-semibold leading-[20px] text-left">
+          {title}
+        </h6>
+        <h4 className="font-montserrat text-[#1E293B] text-[24px] font-medium leading-[36px] text-left">
+          {total}
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default FeeBox;
