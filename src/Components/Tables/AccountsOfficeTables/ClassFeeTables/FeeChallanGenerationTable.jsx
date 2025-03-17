5fd826069e89447221800b88d6b9384a131761d6
usman4222 <usman4243ch@gmail.com>
2025-03-17 23:07:54 +0500

    update tuitionFee

--- src/Components/Tables/AccountsOfficeTables/ClassFeeTables/FeeChallanGenerationTable.jsx
+++ src/Components/Tables/AccountsOfficeTables/ClassFeeTables/FeeChallanGenerationTable.jsx
@@ -31,7 +31,7 @@ const FeeChallanGenerationTable = () => {
       sr: "1",
       class: "7th",
       totalFee: "40,000",
-      tuitionFee: "40,000",
+      tuitionFee: "4,000",
       examFee: "40,000",
       sportsFee: "40,000",
       libraryFee: "40,000",
